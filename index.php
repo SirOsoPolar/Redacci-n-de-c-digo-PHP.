b6f619c0dac9f8fe5d32da8f88eb507b2395fe3a
Hugo Amaya <ogolar@outlook.es>
2020-11-26 10:54:18 -0600

    Este es un commit, subiendo archivos

---
+++ index.php
@@ -0,0 +1,16 @@
+<html>
+<head>
+<title>PRACTICA 1</title>
+<meta name="viewport" content="width=devide-width,user-scalable=no, initial-scal=1.0, minimun-scale=1.0">
+<link rel="stylesheet" href="css/bootstrap.min.css">
+</head>
+<body>
+<div class="card" style="width: 18rem;">
+  <img class="card-img-top" src="imagenes/095.jpg" alt="Card image cap">
+  <div class="card-body">
+    <p class="card-text">Buenos días HUGO ANDRÉS AMAYA CHAIREZ</p>
+  </div>
+</div>
+</form>
+</body>
+</html>
